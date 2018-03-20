514b9b0244d4c7fc064bba84a1d2d63c5fd00ff4
mayuan <mayuanucas@qq.com>
2018-03-20 19:07:04 +0800

    leetcode

---
+++ algorithm/leetcode/021/ac021.go
@@ -0,0 +1,23 @@
+package problem021
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if nil == l1 {
+		return l2
+	}
+	if nil == l2 {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
