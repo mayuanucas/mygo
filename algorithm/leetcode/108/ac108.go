80448017e98fa948b8453eb99c0a102088350f03
mayuan <mayuanucas@qq.com>
2018-03-29 17:11:57 +0800

    有序数组转二叉搜索树

---
+++ algorithm/leetcode/108/ac108.go
@@ -0,0 +1,20 @@
+package problem108
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) < 1 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+}
