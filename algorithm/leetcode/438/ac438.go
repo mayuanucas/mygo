ca640be4e4205e6e288e50c45958d72fdab5db08
mayuan <mayuanucas@qq.com>
2018-04-16 23:50:46 +0800

    Find All Anagrams in a String

--- algorithm/leetcode/438/ac438.go
+++ algorithm/leetcode/438/ac438.go
@@ -1,23 +1,39 @@
 package problem438
 
 func findAnagrams(s string, p string) []int {
-	dict := make(map[byte]bool, len(p))
+	dict := make(map[byte]int, 0)
 	for i := 0; i < len(p); i++ {
-		dict[p[i]] = true
+		dict[p[i]]++
 	}
 
 	ans := make([]int, 0)
-
-	for i := 0; i < len(s); i++ {
-		flag := true
-		for j := 0; j < len(p); j++ {
-			if !dict[s[i+j]] {
-				flag = false
-				break
-			}
+	//two points, initialize count to p's length
+	left, right, count := 0, 0, len(p)
+	for right < len(s) {
+		//move right everytime, if the character exists in p's hash, decrease the count
+		//current hash value >= 1 means the character is existing in p
+		if dict[s[right]] >= 1 {
+			count--
 		}
-		if flag {
-			ans = append(ans, i)
+		dict[s[right]]--
+		right++
+
+		//when the count is down to 0, means we found the right anagram
+		//then add window's left to result list
+		if 0 == count {
+			ans = append(ans, left)
+		}
+
+		//if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
+		//++ to reset the hash because we kicked out the left
+		//only increase the count if the character is in p
+		//the count >= 0 indicate it was original in the hash, cuz it won't go below 0
+		if right-left == len(p) {
+			if v, ok := dict[s[left]]; ok && v >= 0 {
+				count++
+			}
+			dict[s[left]]++
+			left++
 		}
 	}
 
--- algorithm/leetcode/438/ac438_test.go
+++ algorithm/leetcode/438/ac438_test.go
@@ -10,4 +10,12 @@ func Test_findAnagrams(t *testing.T) {
 	p := "abc"
 
 	fmt.Println(findAnagrams(s, p))
+
+	s1 := "abab"
+	p1 := "ab"
+	fmt.Println(findAnagrams(s1, p1))
+
+	s2 := "ababababab"
+	p2 := "aab"
+	fmt.Println(findAnagrams(s2, p2))
 }
