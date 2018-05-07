09a5728d33cfe28927f3de75da9ea3e77021d486
mayuan <mayuanucas@qq.com>
2018-05-07 11:17:43 +0800

    回旋镖的数量

--- algorithm/leetcode/447/ac447.go
+++ algorithm/leetcode/447/ac447.go
@@ -1,3 +1,30 @@
 package problem447
 
+func numberOfBoomerangs(points [][]int) int {
+	ans := 0
 
+	for i := 0; i < len(points); i++ {
+		dict := make(map[int]int, 0)
+		for j := 0; j < len(points); j++ {
+			if i == j {
+				continue
+			}
+
+			d := getDistance(points[i], points[j])
+			dict[d]++
+		}
+
+		for _, v := range dict {
+			ans += v * (v - 1)
+		}
+	}
+
+	return ans
+}
+
+func getDistance(a, b []int) int {
+	dx := a[0] - b[0]
+	dy := a[1] - b[1]
+
+	return dx*dx + dy*dy
+}
