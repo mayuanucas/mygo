9eeb04fafec13bd5eaf9dc3bb1e66fcad03a66dc
mayuan <mayuanucas@qq.com>
2018-05-08 09:19:30 +0800

    最大回文数乘积

---
+++ algorithm/leetcode/479/ac479.go
@@ -0,0 +1,33 @@
+package problem479
+
+import (
+	"strconv"
+	"math"
+)
+
+func largestPalindrome(n int) int {
+	if 1 == n {
+		return 9
+	}
+
+	max := int(math.Pow10(n) - 1)
+	for v := max - 1; v > max/10; v-- {
+		u, _ := strconv.Atoi(strconv.Itoa(v) + reverseInt(v))
+		for x := max; x*x >= u; x-- {
+			if u%x == 0 {
+				return u % 1337
+			}
+		}
+	}
+	return 0
+}
+
+func reverseInt(a int) string {
+	temp := []byte(strconv.Itoa(a))
+	for i, j := 0, len(temp)-1; i < j; {
+		temp[i], temp[j] = temp[j], temp[i]
+		i++
+		j--
+	}
+	return string(temp)
+}
---
+++ algorithm/leetcode/479/ac479_test.go
@@ -0,0 +1,12 @@
+package problem479
+
+import (
+	"testing"
+	"fmt"
+)
+
+func Test_largestPalindrome(t *testing.T) {
+	fmt.Println(largestPalindrome(2))
+	fmt.Println(largestPalindrome(3))
+	fmt.Println(largestPalindrome(4))
+}
