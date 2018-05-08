0daa3529bebef1228b75a2e88d98e06d1911e3ab
mayuan <mayuanucas@qq.com>
2018-05-08 09:20:24 +0800

    反转数组写法

--- algorithm/leetcode/479/ac479.go
+++ algorithm/leetcode/479/ac479.go
@@ -24,10 +24,8 @@ func largestPalindrome(n int) int {
 
 func reverseInt(a int) string {
 	temp := []byte(strconv.Itoa(a))
-	for i, j := 0, len(temp)-1; i < j; {
+	for i, j := 0, len(temp)-1; i < j; i, j = i+1, j-1 {
 		temp[i], temp[j] = temp[j], temp[i]
-		i++
-		j--
 	}
 	return string(temp)
 }
