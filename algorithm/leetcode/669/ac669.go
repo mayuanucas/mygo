12a8e8f7376489ea121d31351eb768a0b1078908
mayuan <mayuanucas@qq.com>
2018-05-20 12:26:16 +0800

    修剪二叉搜索树

---
+++ algorithm/leetcode/669/ac669.go
@@ -0,0 +1,23 @@
+package problem669
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if nil == root {
+		return nil
+	}
+
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
